04fcffd37eae4c3e13fef52394a5b11beb04df9e
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-05-15 18:10:06 +0400

    controller

---
+++ LoyalityProgram/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using LoyalityProgram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoyalityProgram.Controllers
+{
+    [Route("/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("")]
+        public ActionResult<LoyaltyProgramUser> CreateUser(
+        [FromBody] LoyaltyProgramUser user)
+        {
+            if (user == null)
+                return BadRequest();
+
+
+            var newUser = RegisterUser(user);
+            return Created(new Uri($"/users/{newUser.Id}", UriKind.Relative), newUser);
+        }
+        private LoyaltyProgramUser RegisterUser(LoyaltyProgramUser user)
+        {
+            return user;
+        }
+    }
+}
