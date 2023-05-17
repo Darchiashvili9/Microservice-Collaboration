2d033965dcdfed6dde304e3e0bbefdf5c3ed9908
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-05-17 18:42:25 +0400

    docker build

--- LoyalityProgram/EventConsumer/Program.cs
+++ LoyalityProgram/EventConsumer/Program.cs
@@ -9,7 +9,7 @@ var start = await GetStartIdFromDatastore();
 var end = 100;
 var client = new HttpClient();
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-using var resp = await client.GetAsync(new Uri($"http://special-offers:5002/events?start={start}&end={end}"));
+using var resp = await client.GetAsync(new Uri($"http://specialoffers:5002/events?start={start}&end={end}"));
 await ProcessEvents(await resp.Content.ReadAsStreamAsync());
 await SaveStartIdToDataStore(start);
 
