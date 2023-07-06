f4ac67bbcec5109a14e170a1c2570eccf4e4f1e9
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-07-06 16:34:36 +0400

    1

--- LoyalityProgram/EventConsumer/Program.cs
+++ LoyalityProgram/EventConsumer/Program.cs
@@ -10,7 +10,15 @@ Console.WriteLine("******************** Version 2.0 Version 2.0 Version 2.0 Vers
 Console.WriteLine("EVENT CONSUMER ***************** Version 2.0 Version 2.0 Version 2.0");
 
 
+
 var start = await GetStartIdFromDatastore();
+// fake implementation. Should get from a real database
+// ეს რაღაც უაზრო იმპლემენტაცია გამოდის, პირდაპირ შეიძლებოდა აქ ხელით გაწერა მაშინ;
+Task<long> GetStartIdFromDatastore() => Task.FromResult(0L);
+
+
+
+
 var end = 100;
 var client = new HttpClient();
 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -21,8 +29,7 @@ using var resp = await client.GetAsync(new Uri($"http://20.166.179.207:5002/even
 await ProcessEvents(await resp.Content.ReadAsStreamAsync());
 await SaveStartIdToDataStore(start);
 
-// fake implementation. Should get from a real database
-Task<long> GetStartIdFromDatastore() => Task.FromResult(0L);
+
 
 
 // fake implementation. Should apply business rules to events
