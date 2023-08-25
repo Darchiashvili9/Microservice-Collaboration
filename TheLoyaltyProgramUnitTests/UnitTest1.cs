44e0eb27419a09ec5bf510579dedd027935273e5
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-08-25 15:40:50 +0400

    1

---
+++ TheLoyaltyProgramUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TheLoyaltyProgramUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            var tmp = new UnitTest1();
+
+        }
+    }
+}
