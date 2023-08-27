b8f4cc3dd97e667a6bda02e01c642759c97bf9f2
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-08-27 18:30:41 +0400

    1

--- SpecialOffers/Models/EventStore.cs
+++ SpecialOffers/Models/EventStore.cs
@@ -23,4 +23,19 @@
             return ret;
         }
     }
+
+    public class FakeEventStore : IEventStore
+    {
+        public Task RaiseEvent(string name, object content)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<EventFeedEvent>> GetEvents(int start, int end)
+        {
+            if (start > 100)
+                return Task.FromResult(return Task.FromResult(Enumerable.Range(start, end - start)
+                    .Select(i => new EventFeedEvent(i, DateTimeOffset.UtcNow, "some event", new object())));
+        }
+    }
 }
